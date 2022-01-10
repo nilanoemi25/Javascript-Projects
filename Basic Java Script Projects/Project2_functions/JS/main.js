61e19e79360073f708a97fa18227836eed8e1682
nilanoemi25 <nila.noemi25@gmail.com>
2022-01-10 12:11:13 +0000

    Functions
    Using functions and creating them for the first time. Also experience with operators. Module 1.

---
+++ Basic Java Script Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function MyFunction() {   //function
+    var B = 5;  // expression
+    var A = ++B; 
+    document.getElementById("pizza").innerHTML = A; // method on document property 
+  }
+
+  function MyCon(){
+      var soti = "I like eating";   // String in use
+      soti += " pasta and pizza!"; // using operator to concatenate 
+      document.getElementById("concatenate").innerHTML = soti; // using innerHTML to change the HTML 
+  }
+
