0b57b5eb0faf96bda1d4468f1353489d4ea03382
nilanoemi25 <nila.noemi25@gmail.com>
2022-01-10 11:02:33 +0000

    First Assignment
    First Assignment Complete

---
+++ Basic Java Script Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+var Poem = "Poems by Robert Frost"; // Title Alert 
+window.alert(Poem);
+var A = " The Road not Taken", B = "Acquainted with the Night "; // Assigning String variables
+var B = B.fontcolor("pink"); // This is an expression 
+var A = A.fontcolor("blue"); // performing method
+document.write(A+B);  // displaying on Global 
