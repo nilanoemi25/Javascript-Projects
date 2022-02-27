6f56b5f89c73e2a76bb00da19864f04cbd288a73
nilanoemi25 <nila.noemi25@gmail.com>
2022-02-27 14:51:15 +0000

    final
    final add

--- 6/Document Object/JS/main.js
+++ 6/Document Object/JS/main.js
@@ -1,2 +1,16 @@
 console.log(document.URL); 
 // Challenge 24
+
+
+//Challenge 27
+var XML_Parser, XML_Document; 
+
+var Pencil = "<pencil>" + "<colours> red,blue, yellow </colours>" + "<type> HB </type>" + "<year> 2002 </year>" + "</pencil>" ; 
+
+XML_Parser = new DOMParser();
+
+XML_Document = XML_Parser.parseFromString(Pencil, "text/xml"); 
+
+document.getElementById("XML_Example").innerHTML = XML_Document.getElementsByTagName("colours")[0].childNodes[0].nodeValue; 
+
+
